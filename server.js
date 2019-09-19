38a5f51ce3107d44e17ac7ad77bf4a0ba9c35c85
nisol91 <nicolasolzi91@gmail.com>
2019-09-19 13:12:49 +0200

    dev

--- server.js
+++ server.js
@@ -121,5 +121,16 @@ app.use(bodyParser.json());
 // app.use("/project", projectRoute);
 // app.use("/form", formRoute);
 
+//cors supportde
+app.all("*", function(req, res, next) {
+  if (!req.get("Origin")) return next();
+  // use "*" here to accept any origin
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "PUT");
+  res.set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  // res.set('Access-Control-Allow-Max-Age', 3600);
+  if ("OPTIONS" == req.method) return res.send(200);
+  next();
+});
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
